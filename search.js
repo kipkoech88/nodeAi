6e74c6936844bb48fa392ac5e39e04f27d53bfea
kipkoech88 <hoseakipkoech88@gmail.com>
2023-12-17 20:37:56 +0300

    langchain embeddings

---
+++ search.js
@@ -0,0 +1,4 @@
+import {openai} from './openai.js'
+import {Document} from 'langchain/document'
+import {MemoryVectorStore} from 'langchain/vectorstores/memory'
+import {OpenAIEmbeddings} from 'langchain/embeddings/openai'
